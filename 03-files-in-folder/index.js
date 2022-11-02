4bf3cc64a2a196662c2458224508be5967b11858
Alexandr Rylkov <skynerstudent@gmail.com>
2022-11-02 22:20:44 +0300

    feat: refactor 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,15 +4,18 @@ const { readdir } = require('node:fs/promises');
 
 async function getFiles() {
   const files = await readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true });
-    for (const dirent of files) {
-      const fileLink = path.join(__dirname, 'secret-folder', dirent.name);
-      const fileLog = fs.statSync(fileLink);
-      const fileType = path.extname(fileLink);
-      const fileName = path.basename(fileLink, fileType);
-      if (fileLog.isFile()) {
-        console.log(`${fileName} - ${fileType.slice(1)} - ${fileLog.size / 1024}kb`);
-      }
+
+  for (const file of files) {
+    const fileLink = path.join(__dirname, 'secret-folder', file.name);
+    
+    const fileLog = fs.statSync(fileLink);
+    const fileType = path.extname(fileLink);
+    const fileName = path.basename(fileLink, fileType);
+
+    if (fileLog.isFile()) {
+      console.log(`${fileName} - ${fileType.slice(1)} - ${fileLog.size / 1024}kb`);
     }
+  }
 }
 
 getFiles();
